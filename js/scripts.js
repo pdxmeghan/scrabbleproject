330bfd7bcbc802f20fcb9f2c217ae90f73741558
Meghan Lindsley <pdxmeghan@gmail.com>
2014-07-13 11:46:26 -0700

    Adding second spec and full function

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,14 @@ var scrabble = function(letter) {
 	var values = [["a", 1], ["b", 3], ["c", 3], ["d", 2], ["e", 1], ["f", 4], ["g", 2], ["h", 4], ["i", 1], ["j", 8], ["k", 5], ["l", 1], ["m", 3], ["n", 1], ["o", 1], ["p", 3], ["q", 10], ["r", 1], ["s", 1], ["t", 1], ["u", 1], ["v", 4], ["w", 4], ["x", 8], ["y", 4], ["z", 10]];
 	
 
-		for (var i=0; i < values.length; i++) {
-			if (values[i][0] === letterSplit[0]) {
-				total += values[i][1];
-
+	for (var i=0; i < letterSplit.length; i++) {
+		for (var j=0; j < values.length; j++) {
+			if (values[j][0] === letterSplit[i]) {
+				total += values[j][1];
 				break;
 			}
 		} 
+	}
 	var total = total.toString();
 	return total;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe("scrabble", function() {
 	it("will return the value of a letter inputted by the user", function() {
 		scrabble("c").should.equal("3");
 	});
+	it("will return the value of multiple letters inputted by the user", function() {
+		scrabble("cat").should.equal("5");
+	});
 });
